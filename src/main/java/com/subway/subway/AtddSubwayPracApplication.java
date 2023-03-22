49a77fa43295958c87a72865ce6674ba43d5848f
seungwoo.song <seungwoo.song@nhnpayco.com>
2023-03-22 21:04:14 +0900

    Initial commit

---
+++ src/main/java/com/subway/subway/AtddSubwayPracApplication.java
@@ -0,0 +1,13 @@
+package com.subway.subway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddSubwayPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtddSubwayPracApplication.class, args);
+    }
+
+}
