d30c085ba5f2aa03dedbcb8bd131dc9fbe686209
swsong <ssw890720@gmail.com>
2023-04-14 20:07:40 +0900

    fix: builder로 생성시 Sections 초기화 되지 않는 이슈 수정

--- src/main/java/com/subway/subway/line/domian/Line.java
+++ src/main/java/com/subway/subway/line/domian/Line.java
@@ -2,15 +2,16 @@ package com.subway.subway.line.domian;
 
 import com.subway.subway.station.domain.Station;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Builder
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class Line {
 
     @Id
@@ -22,14 +23,25 @@ public class Line {
     private String color;
 
     @Embedded
-    private Sections sections = new Sections();
+    private Sections sections = new Sections(); // 빌더를 통해 생성하면 초기값이 무시되는 이슈있음
 
     private int fare;
 
     public Line(String name, String color, int fare) {
+        this(null, name, color, null, fare);
+    }
+
+    @Builder
+    public Line(Long id, String name, String color, Sections sections, int fare) {
+        this.id = id;
         this.name = name;
         this.color = color;
+        this.sections = createSectionsIfNull(sections);
         this.fare = fare;
+    }
+
+    private Sections createSectionsIfNull(Sections sections) {
+        return (sections == null) ? new Sections() : sections;
     }
 
     public void add(Section section) {
