ea8b14ede5abe46c22c93bf0987f87337cdfa3c5
swsong <ssw890720@gmail.com>
2023-04-14 20:28:02 +0900

    refactor: PathFinder 리팩토링

--- src/main/java/com/subway/subway/line/domian/PathFinder.java
+++ src/main/java/com/subway/subway/line/domian/PathFinder.java
@@ -11,40 +11,64 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PathFinder {
 
-    private final List<Line> lines;
+    private final WeightedMultigraph<Station, SectionEdge> graph;
 
     public static PathFinder of(List<Line> lines) {
-        return new PathFinder(lines);
+        return new PathFinder(createGraph(lines));
     }
 
-    public Path findPath(long source, long target) {
-
+    private static WeightedMultigraph<Station, SectionEdge> createGraph(List<Line> lines) {
         WeightedMultigraph<Station, SectionEdge> graph = new WeightedMultigraph<>(SectionEdge.class);
 
-        // 역 추가
+        // 정점 추가
+        addVertex(graph, lines);
+
+        // 간선 추가
+        addEdge(graph, convertToSectionsList(lines));
+
+        return graph;
+    }
+
+    private static List<Sections> convertToSectionsList(List<Line> lines) {
+        return lines.stream()
+                .map(Line::getSections)
+                .toList();
+    }
+
+    private static void addEdge(WeightedMultigraph<Station, SectionEdge> graph, List<Sections> allSections) {
+        allSections.forEach(sections -> {
+            for (int i = 0; i < sections.size(); i++) {
+                addSectionToGraphByEdge(graph, sections.get(i));
+            }
+        });
+    }
+
+    private static void addSectionToGraphByEdge(WeightedMultigraph<Station, SectionEdge> graph, Section section) {
+        SectionEdge edge = SectionEdge.of(section);
+
+        graph.addEdge(
+                section.getUpStation(),
+                section.getDownStation(),
+                edge);
+
+        graph.setEdgeWeight(edge, section.getDistance());
+    }
+
+    private static void addVertex(WeightedMultigraph<Station, SectionEdge> graph, List<Line> lines) {
         lines.stream()
                 .flatMap(line -> line.getStations().stream())
                 .distinct()
                 .forEach(graph::addVertex);
+    }
 
-        // 탐색할 구간 추가
-        List<Sections> allSections = lines.stream()
-                .map(Line::getSections)
-                .toList();
+    public Path findPath(long source, long target) {
+        GraphPath<Station, SectionEdge> pathResult = new DijkstraShortestPath<>(graph)
+                .getPath(
+                        Station.of(source),
+                        Station.of(target));
 
-        allSections.forEach(sections -> {
-
-            for (int i = 0; i < sections.size(); i++) {
-                Section section = sections.get(i);
-                SectionEdge edge = SectionEdge.of(section);
-                graph.addEdge(section.getUpStation(), section.getDownStation(), edge);
-                graph.setEdgeWeight(edge, section.getDistance());
-            }
-        });
-
-        DijkstraShortestPath<Station, SectionEdge> path = new DijkstraShortestPath<>(graph);
-        GraphPath<Station, SectionEdge> pathResult = path.getPath(Station.of(source), Station.of(target));
-
-        return Path.of((int) pathResult.getWeight(), pathResult.getVertexList());
+        return Path.of(
+                (int) pathResult.getWeight(),
+                pathResult.getVertexList());
     }
 }
