3398c4732cec6e0d9dfbcef7c130c3106312aa19
swsong <ssw890720@gmail.com>
2023-04-15 16:34:49 +0900

    test: 경로탐색 조회 예외 테스트 추가

--- src/test/java/com/subway/subway/line/domian/PathTest.java
+++ src/test/java/com/subway/subway/line/domian/PathTest.java
@@ -1,6 +1,7 @@
 package com.subway.subway.line.domian;
 
-import com.subway.subway.common.exception.CanNotFindPathException;
+import com.subway.subway.common.exception.CanNotFindPathExceptionByNotExistsStation;
+import com.subway.subway.common.exception.CanNotFindPathExceptionBySamePath;
 import com.subway.subway.line.LineFixture;
 import com.subway.subway.station.domain.Station;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,17 +14,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PathTest {
 
-    public static final long STATION_1 = 1L;
-    public static final long STATION_2 = 2L;
-    public static final long STATION_3 = 3L;
-    public static final long STATION_4 = 4L;
-    public SubwayMap subwayMap;
+    private static final long STATION_1 = 1L;
+    private static final long STATION_2 = 2L;
+    private static final long STATION_3 = 3L;
+    private static final long STATION_4 = 4L;
+    private static final long STATION_NOT_CONNECTED_1 = 100L;
+    private static final long STATION_NOT_CONNECTED_2 = 101L;
+    private static final long STATION_NOT_CONNECTED_3 = 102L;
+    private static final long STATION_NOT_EXISTS = 200L;
+
+    private SubwayMap subwayMap;
 
     @BeforeEach
     void setUp() {
-        Line line1 = LineFixture.createLineHas2Section(STATION_1, STATION_2, STATION_3, 2);
-        Line line2 = LineFixture.createLineHas2Section(STATION_1, STATION_4, STATION_3, 3);
-        List<Line> lines = List.of(line1, line2);
+        Line connectedLine1 = LineFixture.createLineHas2Section(STATION_1, STATION_2, STATION_3, 2);
+        Line connectedLine2 = LineFixture.createLineHas2Section(STATION_1, STATION_4, STATION_3, 3);
+        Line notConnectedLine1 = LineFixture.createLineHas2Section(STATION_NOT_CONNECTED_1, STATION_NOT_CONNECTED_2, STATION_NOT_CONNECTED_3, 3);
+        List<Line> lines = List.of(connectedLine1, connectedLine2, notConnectedLine1);
         subwayMap = SubwayMap.of(lines);
     }
 
@@ -47,7 +54,26 @@ class PathTest {
 
     @Test
     void 출발역과_도착역이_같으면_조회_불가() {
-        assertThatThrownBy(() -> subwayMap.findPath(STATION_1, STATION_1)).isInstanceOf(CanNotFindPathException.class);
+        assertThatThrownBy(() -> subwayMap.findPath(STATION_1, STATION_1))
+                .isInstanceOf(CanNotFindPathExceptionBySamePath.class);
+    }
+
+    @Test
+    void 출발역과_도착역이_이어져있지_않으면_조회_불가() {
+        assertThatThrownBy(() -> subwayMap.findPath(STATION_1, STATION_NOT_CONNECTED_1))
+                .isInstanceOf(CanNotFindPathExceptionByNotExistsStation.class);
+    }
+
+    @Test
+    void 없는_도착역을_조회하면_조회_불가() {
+        assertThatThrownBy(() -> subwayMap.findPath(STATION_1, STATION_NOT_EXISTS))
+                .isInstanceOf(CanNotFindPathExceptionByNotExistsStation.class);
+    }
+
+    @Test
+    void 없는_출발역을_조회하면_조회_불가() {
+        assertThatThrownBy(() -> subwayMap.findPath(STATION_NOT_EXISTS, STATION_1))
+                .isInstanceOf(CanNotFindPathExceptionByNotExistsStation.class);
     }
 
     private List<Long> convertToStationIds(Path path) {
