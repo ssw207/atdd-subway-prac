9ebf8bf3b4e4a3715264d4c47278068b920f006f
swsong <ssw890720@gmail.com>
2023-04-15 16:26:26 +0900

    feat: 예외 발생시 메시지 전달할수 있도록 변경

--- src/main/java/com/subway/subway/common/exception/CanNotFindPathException.java
+++ src/main/java/com/subway/subway/common/exception/CanNotFindPathException.java
@@ -1,4 +1,10 @@
 package com.subway.subway.common.exception;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class CanNotFindPathException extends SubwayIllegalArgumentException {
+    public CanNotFindPathException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/subway/subway/common/exception/SubwayIllegalArgumentException.java
+++ src/main/java/com/subway/subway/common/exception/SubwayIllegalArgumentException.java
@@ -1,4 +1,11 @@
 package com.subway.subway.common.exception;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class SubwayIllegalArgumentException extends IllegalArgumentException {
+
+    public SubwayIllegalArgumentException(String message) {
+        super(message);
+    }
 }
