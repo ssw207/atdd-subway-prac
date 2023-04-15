3fead76c098c40857b6e15be501121a119cfd3e2
swsong <ssw890720@gmail.com>
2023-04-15 16:15:55 +0900

    feat: 경로탐색 관련 에러 추가 및 ErrorResponseCode 에서 사용하도록 변경
    각 에러 클래스는 CanNotFindPathException를 상속 받는다

--- src/main/java/com/subway/subway/common/ErrorResponseCode.java
+++ src/main/java/com/subway/subway/common/ErrorResponseCode.java
@@ -1,6 +1,8 @@
 package com.subway.subway.common;
 
-import com.subway.subway.common.exception.CanNotFindPathException;
+import com.subway.subway.common.exception.CanNotFindPathExceptionByNotConnected;
+import com.subway.subway.common.exception.CanNotFindPathExceptionByNotExistsStation;
+import com.subway.subway.common.exception.CanNotFindPathExceptionBySamePath;
 import com.subway.subway.common.exception.CanNotRemoveSectionException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,9 +16,9 @@ import java.util.List;
 public enum ErrorResponseCode {
 
     SECTION_REMOVE_FAIL(HttpStatus.BAD_REQUEST, "1001", "구간을 삭제 할 수 업습니다.", List.of(CanNotRemoveSectionException.class)),
-    CAN_NOT_FIND_PATH_BY_SAME_STATION(HttpStatus.BAD_REQUEST, "2001", "경로를 조회할 수 없습니다. 출발역과 도착역이 같습니다.", List.of(CanNotFindPathException.class)),
-    CAN_NOT_FIND_PATH_BY_NOT_CONNECTED(HttpStatus.BAD_REQUEST, "2002", "경로를 조회할 수 없습니다. 연결되어 있지 않은 역입니다.", List.of(CanNotFindPathException.class)),
-    CAN_NOT_FIND_PATH_BY_NOT_EXISTS_STATION(HttpStatus.BAD_REQUEST, "2003", "경로를 조회할 수 없습니다. 존재하지 않는 역 입니다.", List.of(CanNotFindPathException.class)),
+    CAN_NOT_FIND_PATH_BY_SAME_STATION(HttpStatus.BAD_REQUEST, "2001", "경로를 조회할 수 없습니다. 출발역과 도착역이 같습니다.", List.of(CanNotFindPathExceptionBySamePath.class)),
+    CAN_NOT_FIND_PATH_BY_NOT_CONNECTED(HttpStatus.BAD_REQUEST, "2002", "경로를 조회할 수 없습니다. 연결되어 있지 않은 역입니다.", List.of(CanNotFindPathExceptionByNotConnected.class)),
+    CAN_NOT_FIND_PATH_BY_NOT_EXISTS_STATION(HttpStatus.BAD_REQUEST, "2003", "경로를 조회할 수 없습니다. 존재하지 않는 역 입니다.", List.of(CanNotFindPathExceptionByNotExistsStation.class)),
     COMMON_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "0000", "예외가 발생 했습니다.", List.of(Exception.class));
 
     private final HttpStatus httpStatus;
