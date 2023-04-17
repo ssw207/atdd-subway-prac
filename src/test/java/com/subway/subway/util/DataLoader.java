a72a301837720a497e1fcbf18b7256c89c9bfe22
swsong <ssw890720@gmail.com>
2023-04-17 20:42:32 +0900

    feat: 기본 회원정보를 추가 하는 기능 추가

---
+++ src/test/java/com/subway/subway/util/DataLoader.java
@@ -0,0 +1,20 @@
+package com.subway.subway.util;
+
+import com.subway.subway.member.domain.Member;
+import com.subway.subway.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("test")
+@Component
+@RequiredArgsConstructor
+public class DataLoader {
+
+    private final MemberRepository memberRepository;
+
+    public void loadData() {
+        memberRepository.save(Member.of("admin@email.com", "password"));
+        memberRepository.save(Member.of("member@email.com", "password"));
+    }
+}
