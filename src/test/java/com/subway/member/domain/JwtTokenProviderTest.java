ccdae99fcad1083a9a68595e1592e1137f8fbfc7
swsong <ssw890720@gmail.com>
2023-04-22 22:00:26 +0900

    test: jwt 토큰 검증, 토큰 파싱 테스트 추가

--- src/test/java/com/subway/member/domain/JwtTokenProviderTest.java
+++ src/test/java/com/subway/member/domain/JwtTokenProviderTest.java
@@ -9,6 +9,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class JwtTokenProviderTest {
 
+    public static final String EMAIL = "admin@naver.com";
+    public static final String ROLE = "ROLE_ADMIN";
     private final JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(new JwtProperties("jwt-token-secret-key", 3600000L));
 
     @Test
@@ -16,7 +18,20 @@ class JwtTokenProviderTest {
         assertThat(createToken()).isNotNull();
     }
 
+    @Test
+    void 토큰_검증() {
+        String token = createToken();
+        assertThat(jwtTokenProvider.validateToken(token)).isTrue();
+    }
+
+    @Test
+    void 토큰_파싱() {
+        String token = createToken();
+        assertThat(jwtTokenProvider.getPrincipal(token)).isEqualTo(EMAIL);
+        assertThat(jwtTokenProvider.getRoles(token)).contains(ROLE);
+    }
+
     private String createToken() {
-        return jwtTokenProvider.createToken("test", List.of("ROLE_ADMIN"));
+        return jwtTokenProvider.createToken(EMAIL, List.of(ROLE));
     }
 }
