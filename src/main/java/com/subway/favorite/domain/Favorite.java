5dc273bf96e14f2367c17178be121b194fb0e792
swsong <ssw890720@gmail.com>
2023-04-30 15:28:59 +0900

    fix: 누락된 필드 추가

--- src/main/java/com/subway/favorite/domain/Favorite.java
+++ src/main/java/com/subway/favorite/domain/Favorite.java
@@ -1,6 +1,8 @@
 package com.subway.favorite.domain;
 
+import com.subway.station.domain.Station;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -12,6 +14,29 @@ public class Favorite {
     @Column(name = "favorite_id")
     private Long id;
 
+    @JoinColumn(name = "source_id")
+    @ManyToOne
+    private Station source;
+
+    @JoinColumn(name = "target_id")
+    @ManyToOne
+    private Station target;
+
     @Column(nullable = false)
     private long memberId; // 즐겨찾기에서 회원정보를 조회하는 경우는 없으므로 연관관계를 맺지 않는다.
+
+    @Builder
+    public Favorite(Station source, Station target, long memberId) {
+        this.source = source;
+        this.target = target;
+        this.memberId = memberId;
+    }
+
+    public static Favorite of(Station source, Station target, long memberId) {
+        return Favorite.builder()
+                .source(source)
+                .target(target)
+                .memberId(memberId)
+                .build();
+    }
 }
