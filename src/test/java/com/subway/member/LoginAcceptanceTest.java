65f930da0b64b0d1a22790957adfa6a1c2c6df7b
swsong <ssw890720@gmail.com>
2023-04-30 14:54:46 +0900

    test: 깃허브 인증 인수테스트 추가

--- src/test/java/com/subway/member/LoginAcceptanceTest.java
+++ src/test/java/com/subway/member/LoginAcceptanceTest.java
@@ -2,10 +2,15 @@ package com.subway.member;
 
 import com.subway.common.AcceptanceTest;
 import com.subway.member.dto.JwtTokenResponse;
+import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.subway.common.CommonStep.응답검증;
 import static com.subway.member.fixture.AuthFixture.createJwtTokenRequest;
@@ -25,5 +30,24 @@ public class LoginAcceptanceTest extends AcceptanceTest {
 
         응답검증(response, HttpStatus.OK);
         assertThat(response.as(JwtTokenResponse.class).accessToken()).isNotNull();
+    }
+
+    /**
+     * when: code로 깃허브 인증요청을 하면
+     * then: 토큰이 생성된다
+     */
+    @Test
+    void 깃허브_인증() {
+        Map<String, String> params = new HashMap<>();
+        params.put("code", "832ovnq039hfjn");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(params)
+                .when().post("/login/github")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value()).extract();
+
+        assertThat(response.jsonPath().getString("accessToken")).isNotBlank();
     }
 }
