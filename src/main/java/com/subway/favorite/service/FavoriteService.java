01a48f6dddee7a8854e86eb7041a4f91e2a18dab
swsong <ssw890720@gmail.com>
2023-04-30 15:33:07 +0900

    fix: 즐겨찾기 서비스 잘못된 구현 수정
    station entity 서비스에서 조회하도록 변경
    memberId 주입받도록 변경

--- src/main/java/com/subway/favorite/service/FavoriteService.java
+++ src/main/java/com/subway/favorite/service/FavoriteService.java
@@ -2,6 +2,8 @@ package com.subway.favorite.service;
 
 import com.subway.favorite.domain.Favorite;
 import com.subway.favorite.dto.FavoriteRequest;
+import com.subway.station.domain.Station;
+import com.subway.station.repository.StationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,11 @@ import org.springframework.stereotype.Service;
 public class FavoriteService {
 
     private final FavoriteRepository favoriteRepository;
+    private final StationRepository stationRepository;
 
-    public Favorite save(FavoriteRequest favoriteRequest) {
-        return favoriteRepository.save(favoriteRequest.toEntity());
+    public Favorite save(FavoriteRequest favoriteRequest, long memberId) {
+        Station source = stationRepository.findById(favoriteRequest.source()).orElseThrow(IllegalArgumentException::new);
+        Station target = stationRepository.findById(favoriteRequest.target()).orElseThrow(IllegalArgumentException::new);
+        return favoriteRepository.save(Favorite.of(source, target, memberId));
     }
 }
