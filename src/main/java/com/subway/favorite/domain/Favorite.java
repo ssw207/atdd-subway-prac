33aeefd7b022b7ce4c0f1f3c1504ea6f6218f6de
swsong <ssw890720@gmail.com>
2023-04-30 15:22:48 +0900

    feat: 즐겨찾기 도메인추가

--- src/main/java/com/subway/favorite/domain/Favorite.java
+++ src/main/java/com/subway/favorite/domain/Favorite.java
@@ -11,4 +11,7 @@ public class Favorite {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "favorite_id")
     private Long id;
+
+    @Column(nullable = false)
+    private long memberId; // 즐겨찾기에서 회원정보를 조회하는 경우는 없으므로 연관관계를 맺지 않는다.
 }
