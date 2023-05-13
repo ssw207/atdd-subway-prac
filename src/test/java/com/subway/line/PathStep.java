1946a1622591a15aa9f8d9595b8489a5e90c77f5
swsong <ssw890720@gmail.com>
2023-05-13 10:23:42 +0900

    test: 최단시간 경로를 조회하는 인수테스트 추가

--- src/test/java/com/subway/line/PathStep.java
+++ src/test/java/com/subway/line/PathStep.java
@@ -1,14 +1,15 @@
 package com.subway.line;
 
+import com.subway.line.domian.PathType;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.springframework.http.MediaType;
 
 public class PathStep {
-    public static ExtractableResponse<Response> 지하철_경로조회_요청(Long source, Long target) {
+    public static ExtractableResponse<Response> 지하철_경로조회_요청(Long source, Long target, PathType pathType) {
         return RestAssured.given().log().all()
-                .params("source", source, "target", target)
+                .params("source", source, "target", target, "type", pathType.getCode())
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .get("/paths")
                 .then().log().all()
