897e16adfc7f614a8b88cf8f907398d81dc85071
swsong <ssw890720@gmail.com>
2023-05-14 11:15:28 +0900

    feat: 거리 기반 기본 요금 계산 도메인 추가

---
+++ src/main/java/com/subway/line/domian/FareDistance.java
@@ -0,0 +1,6 @@
+package com.subway.line.domian;
+
+public interface FareDistance {
+    
+    int calculate();
+}
---
+++ src/main/java/com/subway/line/domian/FareDistanceBasic.java
@@ -0,0 +1,9 @@
+package com.subway.line.domian;
+
+public class FareDistanceBasic implements FareDistance {
+
+    @Override
+    public int calculate() {
+        return 1250;
+    }
+}
