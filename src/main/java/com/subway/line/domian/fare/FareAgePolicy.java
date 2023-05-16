33760e3acc6b673d147484c474ab8774c2a49629
swsong <ssw890720@gmail.com>
2023-05-17 08:31:46 +0900

    feat: 청소년 요금정책 로직 추가

--- src/main/java/com/subway/line/domian/fare/FareAgePolicy.java
+++ src/main/java/com/subway/line/domian/fare/FareAgePolicy.java
@@ -5,7 +5,14 @@ import com.subway.line.dto.FareRequestDto;
 public class FareAgePolicy implements FarePolicy {
 
     @Override
-    public int calculate(FareRequestDto distance) {
+    public int calculate(FareRequestDto dto) {
+        int age = dto.age();
+        int totalFare = dto.totalFare();
+
+        if (age >= 13 && age <= 18) {
+            return (int) ((totalFare - 350) * 0.8);
+        }
+
         throw new UnsupportedOperationException(); // TODO 구현 필요
     }
 }
