ad1b09fed8e3353e714bdbf8eab5e631508e8884
swsong <ssw890720@gmail.com>
2023-05-17 08:48:57 +0900

    refactor: 청소년 요금 정책 리팩토링
    요금 계산을 enum으로 위임

--- src/main/java/com/subway/line/domian/fare/FareAgePolicy.java
+++ src/main/java/com/subway/line/domian/fare/FareAgePolicy.java
@@ -1,18 +1,38 @@
 package com.subway.line.domian.fare;
 
 import com.subway.line.dto.FareRequestDto;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.function.IntFunction;
+import java.util.function.IntPredicate;
 
 public class FareAgePolicy implements FarePolicy {
+
+    public static final int DEFAULT_DISCOUNT = 350;
 
     @Override
     public int calculate(FareRequestDto dto) {
         int age = dto.age();
         int totalFare = dto.totalFare();
 
-        if (age >= 13 && age <= 18) {
-            return (int) ((totalFare - 350) * 0.8);
-        }
+        FareAge fareAge = FareAge.of(age);
+        return fareAge.calcFare.apply(totalFare);
+    }
 
-        throw new UnsupportedOperationException(); // TODO 구현 필요
+    @RequiredArgsConstructor
+    enum FareAge {
+        TEEN(age -> age >= 13 && age <= 18, totalFare -> (int) ((totalFare - DEFAULT_DISCOUNT) * 0.8)),
+        ADULT(age -> age >= 19, totalFare -> totalFare);
+
+        public static FareAge of(int age) {
+            return Arrays.stream(values())
+                    .filter(e -> e.match.test(age))
+                    .findAny()
+                    .orElse(ADULT);
+        }
+        
+        private final IntPredicate match; // TODO 람다식 네이밍은 어떻게 해야할까?
+        private final IntFunction<Integer> calcFare;
     }
 }
