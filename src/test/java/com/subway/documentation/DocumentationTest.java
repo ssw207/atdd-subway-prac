342f427f4222a75a1afbf4c2efbff08e3b4ee03a
swsong <ssw890720@gmail.com>
2023-05-22 08:34:11 +0900

    test: 문서화 테스트 공통 로직 추가

---
+++ src/test/java/com/subway/documentation/DocumentationTest.java
@@ -0,0 +1,33 @@
+package com.subway.documentation;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) // defined port로 지정시 인수테스트와 충돌이 발생함
+@ExtendWith(RestDocumentationExtension.class)
+public class DocumentationTest {
+
+    @LocalServerPort
+    private int port;
+
+    protected RequestSpecification spec;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        RestAssured.port = port;
+        this.spec = new RequestSpecBuilder()
+                .addFilter(documentationConfiguration(restDocumentation))
+                .build();
+    }
+}
