8d0e1c63e6940c86e2e33cf4856b1a0c8284f6e5
swsong <ssw890720@gmail.com>
2023-06-06 11:17:21 +0900

    fix: 인수테스트에서 예외발생시 stacktrace 가 출력되지 않는 문제로 공통 예외 핸들러에서 stackTrace를 출력한다

--- src/main/java/com/subway/common/ExceptionHandlerController.java
+++ src/main/java/com/subway/common/ExceptionHandlerController.java
@@ -2,15 +2,18 @@ package com.subway.common;
 
 import com.subway.common.doimain.ErrorResponseCode;
 import com.subway.common.dto.SubwayResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
     @ExceptionHandler
     public ResponseEntity<SubwayResponse> handleCanNotAddSectionException(RuntimeException ex) {
+        ex.printStackTrace();
         ErrorResponseCode code = ErrorResponseCode.from(ex);
         SubwayResponse response = SubwayResponse.of(code, ex.getMessage());
         return new ResponseEntity<>(response, code.getHttpStatus());
